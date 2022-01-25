65c0853396cf34ae9f397996e7da184de97f794d
anylineorg <server@anyline.org>
2022-01-25 16:48:45 +0800

    regular

--- anyline-simple-regular/src/main/java/org/anyline/simple/regular/RegularApplication.java
+++ anyline-simple-regular/src/main/java/org/anyline/simple/regular/RegularApplication.java
@@ -2,6 +2,7 @@ package org.anyline.simple.regular;
 
 import org.anyline.net.HttpResult;
 import org.anyline.net.HttpUtil;
+import org.anyline.util.DateUtil;
 import org.anyline.util.regular.RegularUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,9 +12,20 @@ import java.util.List;
 public class RegularApplication {
     private static Logger log = LoggerFactory.getLogger(RegularApplication.class);
     public static void main(String[] args) throws Exception{
-        start();
+
+
+        //通过正则抽取标签
+        //start();
+
+        //如果不熟悉正则可能通过更简单的方式
+        //截取String
+        cut();
+
+        //如果有许多相同的内容需要提取，如果商品列表中需要提取所有商品url
+        //cuts();
     }
     public static void start() throws Exception{
+
         String url = "http://doc.anyline.org/";
         //请求一个地址
         HttpResult result = HttpUtil.get(url);
@@ -25,7 +37,6 @@ public class RegularApplication {
         }
         //读取html源码
         String html = result.getText();
-
         //获取所有超链接(a标签)
         /*
          * 提取单标签+双标签
@@ -46,7 +57,56 @@ public class RegularApplication {
         //抽取所有 a标签和li标签
         //一定注意:这里的a有可能被包含在li内部，这时的a不会再抽取
         list = RegularUtil.fetchAllTag(html,"a","li");
-
-        //如果
     }
+
+    public static void cut(){
+        //放多情况下我们并不需要复杂的标签内容，只需要截取几个关键字
+        //如提取商品名称和商品价格,而这两个值有可能是根其他内容混在一块的
+        //如以下这段源码
+        String html ="<div class='title' data-product='1001'>商品名称(限时)</div>"
+        +"<div class='price'>一个货币符号:100.00</div>";
+        //这时可以通过字符串截取的方式提取出价格
+        //第0个参数:源数据
+        //第1个到倒数第2个参数:100.00(就是我们要提取的价格) 之前出现的关键字
+        //最后1个参数:100.00之后出现的第1个关键字
+
+        //参数顺序:  源码,k1,k2,k3,kn-1,内容,kn
+
+        String price = RegularUtil.cut(html, "price",":","</div>");
+        log.warn("价格:{}",price);
+
+        //许多情况下price有可能在源码中出现多次，这时需要多个关键字的组合来确认100.00的位置
+
+        html = DateUtil.format("yyyy-MM-dd")+ "<div class='title' data-product='1001'>商品名称(限时)</div>" +
+                "div class='src-price price'></div>" +
+                "<div class='price'>一个货币符号:100.00</div>元";
+
+        price = RegularUtil.cut(html,"src-price","price", "price",":","</div>");
+        log.warn("价格:{}",price);
+
+        //如果需要提取的内容在最后 如上面的单位:元
+        String unit = RegularUtil.cut(html,"src-price","price", "price",":","</div>", RegularUtil.TAG_END);
+        log.warn("单位:{}", unit);
+
+        //同样的如果需要提取的内容在最开始位置 如上面的日期
+
+        String ymd = RegularUtil.cut(html, RegularUtil.TAG_BEGIN, "<");
+        log.warn("日期:{}", ymd);
+    }
+
+
+    //如果有许多相同的内容需要提取，如果商品列表中需要提取所有商品url
+    public static void cuts(){
+        String html = "<li><a href='a.html'>A</a></li>"
+                +"<li><a href='b.html'>B</a></li>"
+                +"<li><a href='c.html'>C</a></li>"
+                +"<li><a href='d.html'>D</a></li>"
+                +"<li><a href='e.html'>E</a></li>"
+                ;
+        List<String> urls = RegularUtil.cuts(html, "href","'","'");
+        for(String url:urls){
+            log.warn("url:{}",url);
+        }
+    }
+
 }
