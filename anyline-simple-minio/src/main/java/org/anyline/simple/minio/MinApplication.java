b4c5b2534b40d7a905262bf8c92c0b51a723b749
anylineorg <server@anyline.org>
2022-06-11 08:57:14 +0800

    minio

---
+++ anyline-simple-minio/src/main/java/org/anyline/simple/minio/MinApplication.java
@@ -0,0 +1,38 @@
+package org.anyline.simple.minio;
+
+import io.minio.messages.Bucket;
+import org.anyline.mimio.util.MinioUtil;
+import org.anyline.service.AnylineService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.anyline","org.anyboot"})
+public class MinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(MinApplication.class);
+        ConfigurableApplicationContext context = application.run(args);
+        MinioUtil util = MinioUtil.getInstance();
+        try{
+
+            List<Bucket> buckets =  util.getAllBuckets();
+            for(Bucket bucket:buckets){
+                System.out.println(bucket.name());
+            }
+            String url = util.putObject("alcdn","a/b.txt",new File("D:\\a.txt"));
+            System.out.println(url);
+               url = MinioUtil.getInstance().getObjectURL("alcdn","a/b.txt");
+            System.out.println(url);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
