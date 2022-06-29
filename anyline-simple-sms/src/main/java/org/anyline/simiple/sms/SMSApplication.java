870ff64c3d7e000097b58b62cbb9f2bf984dde85
anylineorg <server@anyline.org>
2022-06-29 14:07:58 +0800

    阿里云短信模板

---
+++ anyline-simple-sms/src/main/java/org/anyline/simiple/sms/SMSApplication.java
@@ -0,0 +1,33 @@
+package org.anyline.simiple.sms;
+
+import org.anyline.aliyun.sms.util.SMSResult;
+import org.anyline.aliyun.sms.util.SMSUtil;
+import org.anyline.entity.DataRow;
+import org.anyline.util.BeanUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@ComponentScan(basePackages = {"org.anyline"})
+@SpringBootApplication
+public class SMSApplication {
+    public static void main(String[] args) {
+        //签名 和 模板 在这里申请
+        //https://dysms.console.aliyun.com/domestic/text/sign
+        SpringApplication application = new SpringApplication(SMSApplication.class);
+        application.run(args);
+        SMSUtil util = SMSUtil.getInstance();
+        Map<String,String> params = new HashMap<>();
+        params.put("code","000000");
+        DataRow row = new DataRow();
+        row.put("code", "111111");
+        SMSResult result = util.send("签名","SMS_88550009","15800000000", row, "code");
+        System.out.println(BeanUtil.object2json(result));
+        result = util.send("签名","SMS_88550009","15800000000", params);
+        System.out.println(BeanUtil.object2json(result));
+    }
+}
