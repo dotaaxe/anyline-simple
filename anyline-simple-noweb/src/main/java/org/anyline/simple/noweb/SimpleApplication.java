d3cd7bd03f2cac188d56e9a1c3e7884325a2a176
anylineorg <server@anyline.org>
2022-07-06 00:07:30 +0800

    simple

--- anyline-simple-noweb/src/main/java/org/anyline/simple/noweb/SimpleApplication.java
+++ anyline-simple-noweb/src/main/java/org/anyline/simple/noweb/SimpleApplication.java
@@ -1,8 +1,11 @@
 package org.anyline.simple.noweb;
 
 import org.anyboot.jdbc.ds.DynamicDataSourceRegister;
+import org.anyline.entity.DataSet;
+import org.anyline.service.AnylineService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,6 +18,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SimpleApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SimpleApplication.class);
-        application.run(args);
+        ConfigurableApplicationContext ctx = application.run(args);
+        AnylineService service = (AnylineService) ctx.getBean("anyline.service");
+        DataSet set = service.querys("BS_VALUE");
+        System.out.println(set.size());
     }
 }
