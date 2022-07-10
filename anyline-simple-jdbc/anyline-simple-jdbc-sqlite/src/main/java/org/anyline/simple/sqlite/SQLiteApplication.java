c829ba645ba840d270c1aa24bd23e4bfb99c20a0
anylineorg <server@anyline.org>
2022-07-10 21:47:12 +0800

    sqlite

--- anyline-simple-jdbc/anyline-simple-jdbc-sqlite/src/main/java/org/anyline/simple/sqlite/SQLiteApplication.java
+++ anyline-simple-jdbc/anyline-simple-jdbc-sqlite/src/main/java/org/anyline/simple/sqlite/SQLiteApplication.java
@@ -1,9 +1,14 @@
 package org.anyline.simple.sqlite;
 
+
+import org.anyline.entity.DataRow;
+import org.anyline.entity.DataSet;
+import org.anyline.service.AnylineService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @ComponentScan(basePackages = {"org.anyline"})
 @SpringBootApplication
@@ -11,7 +16,19 @@ public class SQLiteApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SQLiteApplication.class);
         ConfigurableApplicationContext context = application.run(args);
-
-
+        org.slf4j.impl.StaticLoggerBinder s;
+        AnylineService service = (AnylineService) context.getBean("anyline.service");
+        String sql = "${DROP TABLE IF EXISTS hr_user ; CREATE TABLE hr_user(ID INT, NAME TEXT)}";
+        service.execute(sql);
+        DataSet users = new DataSet();
+        for(int i=0; i<20;i++) {
+            DataRow user = new DataRow();
+            user.put("ID", (i+1));
+            user.put("NAME", "USER_"+(i+1));
+            users.add(user);
+        }
+        service.insert("hr_user", users);
+        DataSet set = service.querys("hr_user",0,1);
+        System.out.println(set);
     }
 }
