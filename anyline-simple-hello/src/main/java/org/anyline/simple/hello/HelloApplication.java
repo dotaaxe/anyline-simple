c5b3b4adeb8420ef00d152be25e620bbaf26f3e2
anylineorg <server@anyline.org>
2022-07-28 16:30:55 +0800

    hello

--- anyline-simple-hello/src/main/java/org/anyline/simple/hello/HelloApplication.java
+++ anyline-simple-hello/src/main/java/org/anyline/simple/hello/HelloApplication.java
@@ -1,5 +1,6 @@
 package org.anyline.simple.hello;
 
+import org.anyline.entity.DataRow;
 import org.anyline.entity.DataSet;
 import org.anyline.service.AnylineService;
 import org.springframework.boot.SpringApplication;
@@ -19,8 +20,11 @@ public class HelloApplication {
         SpringApplication application = new SpringApplication(HelloApplication.class);
         ConfigurableApplicationContext ctx = application.run(args);
         AnylineService service = (AnylineService) ctx.getBean("anyline.service");
-        DataSet set = service.querys("BS_VALUE");
-        System.out.println(set.size());
+        DataSet set = service.querys("BS_VALUE(ID,CODE,NM,VAL)");
+        System.out.println("查询数量:"+set.size());
+        System.out.println(set.toJSON());
+        set.put("TITLE", "${CODE}-${NM}");
+        System.out.println(set.toJSON());
 
     }
 }
