7b2cb1acb6a77957f70c32e36ee3c1c56a725838
anylineorg <server@anyline.org>
2022-07-28 13:48:03 +0800

    oracle

--- anyline-simple-jdbc/anyline-simple-jdbc-oracle/src/main/java/org/anyline/simple/oracle/OracleApplication.java
+++ anyline-simple-jdbc/anyline-simple-jdbc-oracle/src/main/java/org/anyline/simple/oracle/OracleApplication.java
@@ -1,25 +1,18 @@
 package org.anyline.simple.oracle;
 
-import org.anyboot.jdbc.ds.DynamicDataSourceRegister;
-import org.anyline.entity.DataRow;
-import org.anyline.entity.DataSet;
-import org.anyline.service.AnylineService;
-import org.anyline.util.BasicUtil;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
-
-@ComponentScan(basePackages = {"org.anyline","org.anyboot"})
+@ComponentScan(basePackages = {"org.anyline"})
 @SpringBootApplication
-@Import(DynamicDataSourceRegister.class)
 public class OracleApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(OracleApplication.class);
         ConfigurableApplicationContext context = application.run(args);
-       AnylineService service = (AnylineService) context.getBean("anyline.service");
+        AnylineService service = (AnylineService) context.getBean("anyline.service");
         DataSet set = service.querys("PRE_CASE_INFO");
         DataRow row = service.query("PRE_CASE_INFO");
 
