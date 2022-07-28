1b1e46b34b80db341ac47b1a4a5dcc68703306c1
anylineorg <server@anyline.org>
2022-07-28 13:45:13 +0800

    dm

--- anyline-simple-jdbc/anyline-simple-jdbc-dm/src/main/java/org/anyline/simple/DmApplication.java
+++ anyline-simple-jdbc/anyline-simple-jdbc-dm/src/main/java/org/anyline/simple/DmApplication.java
@@ -5,24 +5,17 @@ import org.anyline.jdbc.ds.DataSourceHolder;
 import org.anyline.service.AnylineService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.anyline"})
-public class Oracle2DmApplication extends SpringBootServletInitializer {
+public class DmApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-
-		return application.sources(Oracle2DmApplication.class);
-	}
 
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(Oracle2DmApplication.class);
+		SpringApplication application = new SpringApplication(DmApplication.class);
 
 		ConfigurableApplicationContext context = application.run(args);
 		AnylineService service = (AnylineService) context.getBean("anyline.service");
