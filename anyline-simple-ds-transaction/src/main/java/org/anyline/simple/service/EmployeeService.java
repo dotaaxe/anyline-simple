32cdd131058d0209ffa9e3a3e3d8091949614696
anylineorg <server@anyline.org>
2022-08-15 18:39:24 +0800

    EmployeeService

---
+++ anyline-simple-ds-transaction/src/main/java/org/anyline/simple/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package org.anyline.simple.service;
+
+import org.anyline.entity.DataRow;
+import org.anyline.service.impl.AnylineServiceImpl;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component("emp.service")
+public class EmployeeService extends AnylineServiceImpl {
+    @Transactional
+    public void insert(DataRow row){
+        insert("HR_EMPLOYEE", row);
+        throw new RuntimeException("test exception");
+    }
+}
