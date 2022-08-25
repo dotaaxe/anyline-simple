0dd5ac10adc7a13c153c07e14db0d61b0fcffc0d
anylineorg <server@anyline.org>
2022-08-25 13:16:53 +0800

    map

--- anyline-simple-baidu-map/src/main/java/BaiduMapApplication.java
+++ anyline-simple-baidu-map/src/main/java/BaiduMapApplication.java
@@ -1,6 +1,6 @@
 import org.anyline.baidu.map.util.BaiduMapConfig;
 import org.anyline.baidu.map.util.BaiduMapUtil;
-import org.anyline.entity.MapPoint;
+import org.anyline.entity.Coordinate;
 import org.anyline.util.BeanUtil;
 
 
@@ -10,7 +10,7 @@ public class BaiduMapApplication {
 		//在这申请 https://lbsyun.baidu.com/apiconsole/key#/home
 		BaiduMapConfig.register("YuFLj**************0PnOzEhBAKh","gKq8Ol3UM********YW7KNXXfdC");
 		BaiduMapUtil util =   BaiduMapUtil.getInstance();
-		MapPoint point = util.regeo("120.3801102573153","36.204793643413055");
+		Coordinate point = util.regeo("120.3801102573153","36.204793643413055");
 		System.out.println(BeanUtil.object2json(point));
 	}
 
--- anyline-simple-qq-map/src/main/java/org/anyline/simple/qq/map/QQMapApplication.java
+++ anyline-simple-qq-map/src/main/java/org/anyline/simple/qq/map/QQMapApplication.java
@@ -1,6 +1,6 @@
 package org.anyline.simple.qq.map;
 
-import org.anyline.entity.MapPoint;
+import org.anyline.entity.Coordinate;
 import org.anyline.qq.map.util.QQMapConfig;
 import org.anyline.qq.map.util.QQMapUtil;
 import org.anyline.util.BeanUtil;
@@ -13,7 +13,7 @@ public class QQMapApplication  {
 		QQMapConfig.register("XTWBZ-XEHKD-*****-*****-CQEBF-PYFGN","gKq8Ol3UM********YW7KNXXfdC");
 		QQMapUtil util =   QQMapUtil.getInstance();
 		//逆地址解析
-		MapPoint point = util.regeo("120.3801102573153","36.204793643413055");
+		Coordinate point = util.regeo("120.3801102573153","36.204793643413055");
 		System.out.println(BeanUtil.object2json(point));
 
 		//IP物理地址解析
