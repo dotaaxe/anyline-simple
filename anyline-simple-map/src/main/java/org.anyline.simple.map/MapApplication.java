46c166294f65b36e6d504347e4120c4d4e7a0a7b
anylineorg <server@anyline.org>
2022-08-25 11:25:46 +0800

    map

--- anyline-simple-map/src/main/java/org.anyline.simple.map/MapApplication.java
+++ anyline-simple-map/src/main/java/org.anyline.simple.map/MapApplication.java
@@ -1,7 +1,7 @@
 package org.anyline.simple.map;
 
 
-import org.anyline.entity.MapPoint;
+import org.anyline.entity.Coordinate;
 import org.anyline.map.MapProxy;
 import org.anyline.util.BeanUtil;
 import org.anyline.util.GISUtil;
@@ -17,7 +17,7 @@ public class MapApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(MapApplication.class);
         ConfigurableApplicationContext context = application.run(args);
-        MapPoint point = MapProxy.regeo(GISUtil.COORD_TYPE.GCJ02LL, "120.378355","36.06256");
-        System.out.println(BeanUtil.object2json(point));
+        Coordinate coordinate = MapProxy.regeo(Coordinate.TYPE.GCJ02LL, "120.378355","36.06256");
+        System.out.println(BeanUtil.object2json(coordinate));
     }
 }
