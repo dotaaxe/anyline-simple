7df4628fc66242c44b514b5cc8cd30891b225351
anylineorg <server@anyline.org>
2022-08-27 17:08:21 +0800

    事务 反例

--- anyline-simple-transaction/README.md
+++ anyline-simple-transaction/README.md
@@ -13,4 +13,9 @@ org.springframework.transaction.annotation.EnableTransactionManagement;
 配置文件中没有不配置
 spring.transaction.rollback-on-commit-failur=true
 
-根据版本的默认配置不同，有些版本不需要配置，不需要注解，自己测试一下  
+根据版本的默认配置不同，有些版本不需要配置，不需要注解，自己测试一下
+
+
+
+不要在controller中回滚 除非能把异常抛出到上一层,也就是抛给spring容器
+示例中的a1是一个失败的反例，并不能实现回滚
--- anyline-simple-transaction/src/main/java/org/anyline/simple/trans/controller/EmployeeController.java
+++ anyline-simple-transaction/src/main/java/org/anyline/simple/trans/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.anyline.entity.DataRow;
 import org.anyline.simple.trans.service.EmployeeService;
 import org.anyline.util.BasicUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,29 @@ public class EmployeeController extends AnylineController {
         }
         int cnt = eservice.count("HR_EMPLOYEE");
         return success("insert前后行数:"+qty+">"+cnt);
+    }
+
+
+    //不要在controller中回滚 除非能把异常抛出到上一层,也就是抛给spring容器
+
+    //以下这样不行
+    @RequestMapping("/a1")
+    public String insert1(){
+        int qty = service.count("HR_EMPLOYEE");
+        try {
+            DataRow row = new DataRow();
+            row.put("NM", BasicUtil.getRandomString(10));
+            insert1(row);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        int cnt = eservice.count("HR_EMPLOYEE");
+        return success("insert前后行数:"+qty+">"+cnt);
+    }
+
+    @Transactional
+    public void insert1(DataRow row){
+        service.insert("HR_EMPLOYEE", row);
+        throw  new RuntimeException("test RuntimeException");
     }
 }
