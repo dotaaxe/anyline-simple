fa4c0d86b86374098f7fe073ffdb411850869e35
anylineorg <server@anyline.org>
2022-09-02 01:11:49 +0800

    pom

--- anyline-simple-rabbitmq/src/main/java/org/anyline/simple/mq/MQApplication.java
+++ anyline-simple-rabbitmq/src/main/java/org/anyline/simple/mq/MQApplication.java
@@ -1,33 +1,15 @@
 package org.anyline.simple.mq;
 
-import com.rabbitmq.client.*;
-import org.anyline.entity.DataRow;
-import org.anyline.entity.DataSet;
-import org.anyline.jdbc.ds.DataSourceHolder;
-import org.anyline.net.HttpUtil;
-import org.anyline.service.AnylineService;
-import org.anyline.util.BasicUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-
-import java.io.File;
-import java.io.IOException;
 
 @ComponentScan(basePackages = {"org.anyline"})
 @SpringBootApplication
-public class MQApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-
-		return application.sources(MQApplication.class);
-	}
+public class MQApplication  {
 
 
 	public static void main(String[] args) {
