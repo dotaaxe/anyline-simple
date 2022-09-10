dd23454a56ee85969ae8c01961ef3ce76ad9e544
anylineorg <server@anyline.org>
2022-09-11 00:32:07 +0800

    SQL Server

---
+++ anyline-simple-jdbc-dialect/anyline-simple-jdbc-mssql/src/main/java/org/anyline/simple/mssql/MSSQLApplication.java
@@ -0,0 +1,23 @@
+package org.anyline.simple.mssql;
+
+
+import org.anyline.entity.DataSet;
+import org.anyline.entity.PageNaviImpl;
+import org.anyline.service.AnylineService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"org.anyline"})
+@SpringBootApplication
+public class MSSQLApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(MSSQLApplication.class);
+        ConfigurableApplicationContext context = application.run(args);
+        AnylineService service = (AnylineService) context.getBean("anyline.service");
+        DataSet set = service.querys("ABM100T", new PageNaviImpl().setCurPage(2).setPageRows(10));
+        System.out.println(set);
+
+    }
+}
