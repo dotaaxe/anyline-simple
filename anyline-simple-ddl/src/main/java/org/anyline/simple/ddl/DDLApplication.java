bb30398ff890ce640e073eb012b46ae063289460
anylineorg <server@anyline.org>
2022-09-12 12:33:02 +0800

    DDListener说明

--- anyline-simple-ddl/src/main/java/org/anyline/simple/ddl/DDLApplication.java
+++ anyline-simple-ddl/src/main/java/org/anyline/simple/ddl/DDLApplication.java
@@ -1,16 +1,21 @@
 package org.anyline.simple.ddl;
 
+import org.anyboot.jdbc.ds.DynamicDataSourceRegister;
 import org.anyline.entity.DataRow;
+import org.anyline.jdbc.ds.DataSourceHolder;
 import org.anyline.jdbc.entity.Column;
+import org.anyline.jdbc.entity.Table;
 import org.anyline.service.AnylineService;
 import org.anyline.util.DateUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.anyline"})
+@ComponentScan(basePackages = {"org.anyline","org.anyboot"})
+@Import(DynamicDataSourceRegister.class)
 public class DDLApplication {
 
 	private static AnylineService service;
@@ -21,24 +26,46 @@ public class DDLApplication {
 		ConfigurableApplicationContext context = application.run(args);
 
 		service = context.getBean(AnylineService.class);
-		//init();
+		table();
+		//column();
 		//执行异常监听
-		exception();
+		//exception();
+		System.out.println("\n============================= PG ==========================================\n");
+		service.tables();
+		DataSourceHolder.setDataSource("pg");
+		table();
 	}
-
+	public static void table() throws Exception{
+		Table table = new Table();
+		table.setName("A_TEST_AAAA");
+		table.addColumn("ID", "int").setPrimaryKey(true).setAutoIncrement(true).setComment("commnet");
+		table.addColumn("NAME","varchar(50)");
+		table.addColumn("NAMESS","varchar(50)");
+		service.ddl().save(table);
+	}
 	public static void exception() throws Exception{
-	//ConfigTable.AFTER_ALTER_COLUMN_EXCEPTION_ACTION
-	// 0:中断执行
-	// 1:直接修正
-	// n:行数<n时执行修正  >n时触发另一个监听(默认返回false)
+		//ConfigTable.AFTER_ALTER_COLUMN_EXCEPTION_ACTION
+		// 0:中断执行
+		// 1:直接修正
+		// n:行数<n时执行修正  >n时触发另一个监听(默认返回false)
+
+
+		//表中有数据的情况下
+		DataRow row = new DataRow();
+		row.put("A_CHAR","123A");
+		service.insert("test", row);
 
 		Column column = new Column();
 		column.setTable("test");
 		column.setName("A_CHAR");
+		//这一列原来是String类型 现在改成int类型
+		//如果值不能成功实现殷式转换(如123A转换成int)会触发一次默认的DDListener.afterAlterException
+		//如果afterAlterException返回true，会再执行一次alter column如果还失败就会抛出异常
+		//如果不用默认listener可以column.setListener
 		column.setTypeName("int");
 		service.ddl().save(column);
 	}
-	public static void init() throws Exception{
+	public static void column() throws Exception{
 
 		Column column = new Column();
 		column.setTable("test");
@@ -68,11 +95,6 @@ public class DDLApplication {
 
 		service.ddl().drop(column);
 
-		//表中有数据的情况下
-		DataRow row = new DataRow();
-		row.put("A_CHAR","123");
-		service.insert("test", row);
-		column = new Column();
 
 	}
 }
