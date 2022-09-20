e672ac9fbbdabc0419052532805776a2769bc53d
anylineorg <server@anyline.org>
2022-09-20 23:44:04 +0800

    ORACLE junit

--- anyline-simple-jdbc-dialect/anyline-simple-jdbc-oracle/src/main/java/org/anyline/simple/oracle/OracleApplication.java
+++ anyline-simple-jdbc-dialect/anyline-simple-jdbc-oracle/src/main/java/org/anyline/simple/oracle/OracleApplication.java
@@ -18,45 +18,7 @@ import java.util.List;
 public class OracleApplication {
     public static void main(String[] args) throws Exception{
         SpringApplication application = new SpringApplication(OracleApplication.class);
-        ConfigurableApplicationContext context = application.run(args);
-        AnylineService service = (AnylineService) context.getBean("anyline.service");
-        Table table = service.metadata().table("a_test");
-        if(null != table){
-            service.ddl().drop(table);
-        }
-        table = new Table("a_test");
-        table.addColumn("ID", "int");
-        table.addColumn("NAME","varchar2(20)");
-        service.ddl().save(table);
-        DataSet set = new DataSet();
-        for(int i=1; i<=10; i++){
-            DataRow row = new DataRow();
-            row.put("ID", i);
-            row.put("NAME", "N"+i);
-            set.add(row);
-        }
-        service.insert("a_test", set);
-        List<User> list = new ArrayList<>();
-        for(int i=11; i<=20; i++){
-            User user = new User();
-            user.setId(i);
-            user.setName("NAME-"+i);
-            list.add(user);
-            //service.insert("a_test", user);
-        }
-        service.insert("a_test", list, false);
-
-
-        set = service.querys("a_test");
-        DataRow row = service.query("a_test");
-
-
-
-        row.remove("ROW_NUMBER");
-        set.remove("ROW_NUMBER");
-        set.setPrimaryKey("ID");
-        service.delete(set);
-        System.out.println(set);
+        application.run(args);
 
     }
 }
