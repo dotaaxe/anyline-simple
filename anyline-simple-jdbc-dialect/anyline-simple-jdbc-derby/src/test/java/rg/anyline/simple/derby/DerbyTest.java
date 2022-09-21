90aedc03fc3e0aab0ba776452b63d7f0468cccd8
anylineorg <server@anyline.org>
2022-09-21 13:09:12 +0800

    derby test

--- anyline-simple-jdbc-dialect/anyline-simple-jdbc-derby/src/test/java/rg/anyline/simple/derby/DerbyTest.java
+++ anyline-simple-jdbc-dialect/anyline-simple-jdbc-derby/src/test/java/rg/anyline/simple/derby/DerbyTest.java
@@ -1,5 +1,4 @@
-package org.anyline.simple.derby;
-
+package rg.anyline.simple.derby;
 
 import org.anyline.entity.DataRow;
 import org.anyline.entity.DataSet;
@@ -11,62 +10,51 @@ import org.anyline.service.AnylineService;
 import org.anyline.util.BasicUtil;
 import org.anyline.util.ConfigTable;
 import org.anyline.util.LogUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 
+@SpringBootTest
+public class DerbyTest {
+    private Logger log = LoggerFactory.getLogger(DerbyTest.class);
+    @Autowired
+    private AnylineService service          ;
+    @Autowired
+    private JdbcTemplate jdbc               ;
+    private String catalog  = null          ; // 默认数据库名
+    private String schema   = null          ; // 默认PUBLIC
+    private String table    = "CRM_USER"    ; // 表名
 
-@ComponentScan(basePackages = {"org.anyline"})
-@SpringBootApplication 
-public class DerbyApplication {
-    private static Logger log = LoggerFactory.getLogger(DerbyApplication.class);
-    private static AnylineService service      ;
-    private static JdbcTemplate jdbc               ;
-    private static String catalog  = null          ; // 默认数据库名
-    private static String schema   = null          ; // 默认PUBLIC
-    private static String table    = "CRM_USER"    ; // 表名
 
-    public static void main(String[] args) throws Exception{
-        SpringApplication application = new SpringApplication(DerbyApplication.class);
-        ConfigurableApplicationContext context = application.run(args);
-        org.slf4j.impl.StaticLoggerBinder s;
-        service = context.getBean(AnylineService.class);
-        jdbc = context.getBean(JdbcTemplate.class);
-        ddl();
-        dml();
-        help();
-
-    }
-
-    public static void ddl() throws Exception{
+    @Test
+    public void ddl() throws Exception{
         ConfigTable.IS_THROW_SQL_EXCEPTION = true; //遇到SQL异常直接抛出
         //检测表结构
-        Table table = service.metadata().table(catalog, schema,DerbyApplication.table);
+        Table table = service.metadata().table(catalog, schema, this.table);
         //如果存在则删除
         if(null != table){
             service.ddl().drop(table);
         }
         //也可以直接删除(需要数据库支持 IF EXISTS)
-        //service.ddl().drop(new Table(catalog, schema,DerbyApplication.table));
+        service.ddl().drop(new Table(catalog, schema, this.table));
 
         //再查询一次
-        table = service.metadata().table(catalog, schema,DerbyApplication.table);
-        //Assertions.assertNull(table);
+        table = service.metadata().table(catalog, schema, this.table);
+        Assertions.assertNull(table);
 
         //定义表结构
-        table = new Table(catalog, schema,DerbyApplication.table);
+        table = new Table(catalog, schema, this.table);
 
         //添加列
         //自增长列 如果要适配多种数据库 setAutoIncrement 有必须的话可以设置起始值与增量 setAutoIncrement(int seed, int step)
-        table.addColumn("ID", "BIGINT", false, null).setComment("主键").setAutoIncrement(true).setPrimaryKey(true);
+        table.addColumn("ID", "INT", false, null).setComment("主键").setAutoIncrement(true).setPrimaryKey(true);
         table.addColumn("CODE", "VARCHAR(50)").setComment("编号");
         table.addColumn("NAME", "VARCHAR(50)").setComment("名称");
         //默认当前时间 如果要适配多种数据库 用 SQL_BUILD_IN_VALUE.CURRENT_TIME
@@ -77,10 +65,11 @@ public class DerbyApplication {
         service.ddl().create(table);
 
         //再查询一次
-        table = service.metadata().table(catalog, schema,DerbyApplication.table);
-        //Assertions.assertNotNull(table);
+        table = service.metadata().table(catalog, schema, this.table);
+        Assertions.assertNotNull(table);
     }
-    public static void dml() throws Exception{
+    @Test
+    public void dml() throws Exception{
         DataSet set = new DataSet();
         for(int i=1; i<=10; i++){
             DataRow row = new DataRow();
@@ -90,25 +79,25 @@ public class DerbyApplication {
         }
         int qty = service.insert(table, set);
         log.warn(LogUtil.format("[insert result][插入数量:{}]", 36), qty);
-        //Assertions.assertEquals(qty , 10);
+        Assertions.assertEquals(qty , 10);
 
         //查询全部数据
         set = service.querys(table);
         log.warn(LogUtil.format("[query result][查询数量:{}]", 36), set.size());
         log.warn("[多行查询数据]:{}",set.toJSON());
-        //Assertions.assertEquals(set.size() , 10);
+        Assertions.assertEquals(set.size() , 10);
 
         //只查一行
         DataRow row = service.query(table);
         log.warn("[单行查询数据]:{}",row.toJSON());
-        //Assertions.assertNotNull(row);
-        //Assertions.assertEquals(row.getId(), "1");
+        Assertions.assertNotNull(row);
+        Assertions.assertEquals(row.getId(), "1");
 
         //查最后一行
         row = service.query(table, "ORDER BY ID DESC");
         log.warn("[单行查询数据]:{}",row.toJSON());
-        //Assertions.assertNotNull(row);
-        //Assertions.assertEquals(row.getInt("ID",10), 10);
+        Assertions.assertNotNull(row);
+        Assertions.assertEquals(row.getInt("ID",10), 10);
 
         //更新
         //put覆盖了Map.put返回Object
@@ -132,8 +121,8 @@ public class DerbyApplication {
         set = service.querys(table, page);
         log.warn(LogUtil.format("[分页查询][共{}行 第{}/{}页]", 36), page.getTotalRow(), page.getCurPage(), page.getTotalPage());
         log.warn(set.toJSON());
-        //Assertions.assertEquals(page.getTotalPage() , 4);
-        //Assertions.assertEquals(page.getTotalRow() , 10);
+        Assertions.assertEquals(page.getTotalPage() , 4);
+        Assertions.assertEquals(page.getTotalRow() , 10);
 
         service.update(row,"NAME");
         //根据主键删除
@@ -141,10 +130,11 @@ public class DerbyApplication {
 
         qty = service.count(table);
         log.warn(LogUtil.format("[总数统计][count:{}]", 36), qty);
-        //Assertions.assertEquals(qty , 9);
+        Assertions.assertEquals(qty , 9);
     }
 
-    public static void help() throws Exception{
+    @Test
+    public void help() throws Exception{
         ResultSet set = jdbc.getDataSource().getConnection().getMetaData().getTables(null, null, table, "TABLE".split(","));
         ResultSetMetaData md = set.getMetaData();
         if (set.next()) {
