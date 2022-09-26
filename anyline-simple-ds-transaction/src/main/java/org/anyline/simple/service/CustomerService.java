badbf5096b5468736a2adbeb0f945fd2795413b2
anylineorg <server@anyline.org>
2022-09-26 21:03:30 +0800

    CustomerService

--- anyline-simple-ds-transaction/src/main/java/org/anyline/simple/service/CustomerService.java
+++ anyline-simple-ds-transaction/src/main/java/org/anyline/simple/service/CustomerService.java
@@ -1,12 +1,12 @@
 package org.anyline.simple.service;
 
 import org.anyline.entity.DataRow;
-import org.anyline.service.impl.AnylineServiceImpl;
+import org.anyline.service.init.SimpleService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component("cust.service")
-public class CustomerService extends AnylineServiceImpl {
+public class CustomerService extends SimpleService {
     @Transactional
     public void insert(DataRow row){
         insert("<crm>crm_customer", row);
