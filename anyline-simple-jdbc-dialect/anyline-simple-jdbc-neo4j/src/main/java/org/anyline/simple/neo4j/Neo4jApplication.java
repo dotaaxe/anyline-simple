4d5fbd5d0efbf3d85a5238ef678f2d3b8b5d5d6d
anylineorg <server@anyline.org>
2022-09-26 21:03:12 +0800

    neo4j junit

--- anyline-simple-jdbc-dialect/anyline-simple-jdbc-neo4j/src/main/java/org/anyline/simple/neo4j/Neo4jApplication.java
+++ anyline-simple-jdbc-dialect/anyline-simple-jdbc-neo4j/src/main/java/org/anyline/simple/neo4j/Neo4jApplication.java
@@ -7,6 +7,9 @@ import org.anyline.entity.PageNavi;
 import org.anyline.entity.PageNaviImpl;
 import org.anyline.entity.adapter.KeyAdapter;
 import org.anyline.service.AnylineService;
+import org.anyline.util.LogUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -21,115 +24,11 @@ import java.util.List;
 @ComponentScan(basePackages = {"org.anyline"})
 @SpringBootApplication
 public class Neo4jApplication {
-
+    private static Logger log = LoggerFactory.getLogger(Neo4jApplication.class);
     public static void main(String[] args) throws Exception{
         SpringApplication application = new SpringApplication(Neo4jApplication.class);
         ConfigurableApplicationContext context = application.run(args);
-        AnylineService service = context.getBean(AnylineService.class);
-        DataRow.DEFAULT_KEY_KASE = KeyAdapter.KEY_CASE.SRC;
-        JdbcTemplate jdbc = context.getBean(JdbcTemplate.class);
-
-        //当前版本(8.5.7)只支持原生CQL执行，不支持占位符
-        //基本上只会用到execute与querys
-        //分页请自己查询总数
-
-        //以下说到的标签相当于关系型数据库中的表
-        //原生CQL创建节点
-/*        service.execute("create (d:Dept{name:'财务部', leader:'张三'})");
-        //创建多标签(表)节点
-        service.execute("CREATE (e:User:Employee{NAME:'张三',AGE:20})");
-        //创建多节点
-        service.execute("CREATE (e:User{NAME:'张三',AGE:20})");*/
-
-        //根据DataRow创建节点
-        DataRow r = new DataRow();
-        r.put("NAME","ZH");
-        r.put("AGE", 20);
-        service.insert("user", r);
-        //创建多标签(表)节点
-   /*     service.insert("User:Employee", r);
-
-        //根据entity创建节点
-        User u = new User("u1", 10);
-        service.insert(u);
-        //临时指定标签(表)
-        service.insert("set_user",u);
-        //指定多标签(表)
-        service.insert("User:Employee", r);
 
 
-        //根据DataSet创建节点
-        DataSet set = new DataSet();
-        for(int i=1; i<=10; i++){
-            DataRow row = new DataRow();
-            row.put("NAME", "user"+i);
-            row.put("AGE", i*5);
-            set.add(row);
-        }
-        service.insert("Batch", set);
-        List<User> users = new ArrayList<>();
-        for(int i=1; i<=10; i++){
-            users.add(new User("euser"+i, i*3));
-        }
-        service.insert(users);
-        service.insert("Batch", (Object) users);
-
-
-        //合计
-        int total = service.count("Dept","leader:张三");
-        System.out.println("total:"+total);
-        //节点是否存在
-        boolean exists = service.exists("Dept","leader:张三");
-        System.out.println("exists:"+exists);
-
-
-        //查询
-        set = service.querys("match (s:Dept) return s.name, s.qty limit 3 ");
-        for(DataRow row:set){
-            System.out.println(row);
-        }
-        //按条件查询
-        set = service.querys("Dept","leader:张三");
-        for(DataRow row:set){
-            System.out.println(row);
-        }
-
-        //按条件分页查询
-        PageNavi navi = new PageNaviImpl(3, 10);
-        set = service.querys("Dept", navi, "leader:张三");
-        for(DataRow row:set){
-            System.out.println(row);
-        }
-        set = service.querys("Dept", 0, 9, "leader:张三");
-        for(DataRow row:set){
-            System.out.println(row);
-        }
-
-        set = service.querys("match (s:Dept) return s.name limit 3 ");
-        for(DataRow row:set){
-            System.out.println(row);
-        }*/
-
-/*
-        DataSet set = service.querys("match (s:Dept) return s,s.name as name limit 100 ");
-        set = service.querys("MATCH (e:Person)-[k:KNOWS]-(f) where e.name='Johan' RETURN e,k, f");
-        //返回{e={},k={},f={}}
-
-        //service.querys("Person",0,10,"name:Johan");
-        for(DataRow row:set){
-            System.out.println(row);
-        }
-        DataRow row = new DataRow("User");
-        row.put("name","张三");
-        row.put("age", 20);*/
-       // service.save(row);
-/*
-        Statement stmt = jdbc.getDataSource().getConnection().createStatement();
-        ResultSet rs = stmt.executeQuery("create (n0:Person { name: 'S' }) return id(n0) as id");
-        System.out.println(rs);
-        while(rs.next())
-        {
-            System.out.println(rs.getString("id"));
-        }*/
     }
 }
---
+++ anyline-simple-jdbc-dialect/anyline-simple-jdbc-neo4j/src/test/java/org/anyline/simple/neo4j/Neo4jTest.java
@@ -0,0 +1,150 @@
+package org.anyline.simple.neo4j;
+
+import org.anyline.entity.DataRow;
+import org.anyline.entity.DataSet;
+import org.anyline.entity.PageNavi;
+import org.anyline.entity.PageNaviImpl;
+import org.anyline.entity.adapter.KeyAdapter;
+import org.anyline.service.AnylineService;
+import org.anyline.util.BasicUtil;
+import org.anyline.util.BeanUtil;
+import org.anyline.util.LogUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class Neo4jTest {
+    private Logger log = LoggerFactory.getLogger(Neo4jTest.class);
+    @Autowired
+    private AnylineService service          ;
+    @Autowired
+    private JdbcTemplate jdbc               ;
+    private String catalog  = null          ; //
+    private String schema   = null          ; //
+    private String table    = "CRM_USER"    ; // 表名
+
+
+    @Test
+    public void dml() throws Exception{
+        DataRow.DEFAULT_KEY_KASE = KeyAdapter.KEY_CASE.SRC;
+        //以下说到的标签相当于关系型数据库中的表
+        //原生CQL创建节点
+        service.execute("create (d:Dept{name:'财务部', leader:'张三'})");
+        //创建多标签(表)节点
+        service.execute("CREATE (e:User:Employee{NAME:'张三',AGE:20})");
+        //创建多节点
+        service.execute("CREATE (e:User{NAME:'张三',AGE:20})");
+
+        //根据DataRow创建节点
+        DataRow r = new DataRow();
+        r.put("NAME","ZH");
+        r.put("AGE", 20);
+        int qty = service.insert("user", r);
+        log.warn(LogUtil.format("[DataRow单行插入][插入数量:{}][生成主键:{}]", 36), qty, r.getId());
+        Assertions.assertEquals(qty, 1);
+        Assertions.assertNotNull(r.getId());
+        //创建多标签(表)节点
+        service.insert("User:Employee", r);
+        log.warn(LogUtil.format("[DataRow多标签单行插入][插入数量:{}][生成主键:{}]", 36), qty, r.getId());
+        Assertions.assertEquals(qty, 1);
+        Assertions.assertNotNull(r.getId());
+
+        //根据entity创建节点
+        User u = new User("u1", 10);
+        service.insert(u);
+        log.warn(LogUtil.format("[Entity单行插入][插入数量:{}][生成主键:{}]", 36), qty, u.getId());
+        Assertions.assertEquals(qty, 1);
+        Assertions.assertNotNull(u.getId());
+
+        //临时指定标签(表)
+        service.insert("set_user",u);
+        log.warn(LogUtil.format("[Entity单行插入][临时指定目标表][插入数量:{}][生成主键:{}]", 36), qty, u.getId());
+        Assertions.assertEquals(qty, 1);
+        Assertions.assertNotNull(u.getId());
+
+        //指定多标签(表)
+        service.insert("User:Employee", u);
+        log.warn(LogUtil.format("[Entity多标签单行插入][插入数量:{}][生成主键:{}]", 36), qty, u.getId());
+        Assertions.assertEquals(qty, 1);
+        Assertions.assertNotNull(u.getId());
+
+
+        //根据DataSet创建节点
+        DataSet set = new DataSet();
+        for(int i=1; i<=10; i++){
+            DataRow row = new DataRow();
+            row.put("NAME", "user"+i);
+            row.put("AGE", i*5);
+            set.add(row);
+        }
+        qty = service.insert("Batch", set);
+        log.warn(LogUtil.format("[DataSet批量插入][插入数量:{}][生成主键:{}]", 36), qty, set.getStrings("ID"));
+        Assertions.assertEquals(qty, set.size());
+        Assertions.assertEquals(set.getStrings("ID").size(), set.size());
+
+        List<User> users = new ArrayList<>();
+        for(int i=1; i<=10; i++){
+            users.add(new User("euser"+i, i*3));
+        }
+        qty = service.insert(users);
+        log.warn(LogUtil.format("[List<Entity>批量插入][插入数量:{}]", 36), qty);
+        Assertions.assertEquals(qty, users.size());
+
+        qty = service.insert("Batch", (Object) users);
+        log.warn(LogUtil.format("[List<Entity>批量插入临时指定表][插入数量:{}][users:{}]", 36), qty, BeanUtil.object2json(users));
+        Assertions.assertEquals(qty, users.size());
+
+        //合计
+        int total = service.count("Dept","leader:张三");
+        log.warn(LogUtil.format("[合计数量][total:{}]", 36), total);
+
+        total = service.count("");
+        log.warn(LogUtil.format("[整库合计数量][total:{}]", 36), total);
+
+        //节点是否存在
+        boolean exists = service.exists("Dept","leader:张三");
+        log.warn(LogUtil.format("[检测节点][exists:{}]", 36), exists);
+
+        //查询
+        set = service.querys("match (s:Dept) return s.name, s.qty limit 3 ");
+        log.warn(LogUtil.format("[原生CQL查询][result:{}]", 36), set.toJSON());
+
+        //按条件查询,查询结果不含前缀
+        set = service.querys("Dept","leader:张三","id:>0");
+        log.warn(LogUtil.format("[按条件查询][result:{}]", 36), set.toJSON());
+
+        //按条件分页查询
+        PageNavi navi = new PageNaviImpl(3, 10);
+        set = service.querys("Dept", navi, "leader:张三");
+        log.warn(LogUtil.format("[按条件分页查询][result:{}]", 36), set.toJSON());
+
+        //指定截取
+        set = service.querys("Dept", 0, 9, "leader:张三");
+        log.warn(LogUtil.format("[指定截取][result:{}]", 36), set.toJSON());
+
+        set = service.querys("match (s:Dept) return s.name limit 3 ");
+        log.warn(LogUtil.format("[原生CQL][result:{}]", 36), set.toJSON());
+
+
+        set = service.querys("match (s:Dept) return s, s.name as name limit 10 ");
+
+        log.warn(LogUtil.format("[原生CQL][result:{}]", 36), set.toJSON());
+
+        set = service.querys("MATCH (e:Person)-[k:KNOWS]-(f) where e.name='Johan' RETURN e,k, f");
+        //返回{e={},k={},f={}}
+        log.warn(LogUtil.format("[原生CQL][result:{}]", 36), set.toJSON());
+
+    }
+}
