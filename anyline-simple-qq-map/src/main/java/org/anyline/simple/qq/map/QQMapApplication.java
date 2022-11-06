bcba6ba7a4f509fbdd9a20429fd297367c779427
anyline <server@anyline.org>
2022-11-06 11:16:09 +0800

    QQMapClient

--- anyline-simple-qq-map/src/main/java/org/anyline/simple/qq/map/QQMapApplication.java
+++ anyline-simple-qq-map/src/main/java/org/anyline/simple/qq/map/QQMapApplication.java
@@ -1,8 +1,8 @@
 package org.anyline.simple.qq.map;
 
 import org.anyline.entity.Coordinate;
+import org.anyline.qq.map.util.QQMapClient;
 import org.anyline.qq.map.util.QQMapConfig;
-import org.anyline.qq.map.util.QQMapUtil;
 import org.anyline.util.BeanUtil;
 
 
@@ -11,7 +11,7 @@ public class QQMapApplication  {
 	public static void main(String[] args) {
 		//在这申请 https://lbs.qq.com/dev/console/application/mine-
 		QQMapConfig.register("XTWBZ-XEHKD-*****-*****-CQEBF-PYFGN","gKq8Ol3UM********YW7KNXXfdC");
-		QQMapUtil util =   QQMapUtil.getInstance();
+		QQMapClient util =   QQMapClient.getInstance();
 		//逆地址解析
 		Coordinate point = util.regeo("120.3801102573153","36.204793643413055");
 		System.out.println(BeanUtil.object2json(point));
