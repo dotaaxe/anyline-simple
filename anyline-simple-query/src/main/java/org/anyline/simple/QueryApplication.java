e22b9d31ad8716471436cb3711d259ee4ed3b419
anyline <server@anyline.org>
2022-11-06 11:12:53 +0800

    query

--- anyline-simple-query/src/main/java/org/anyline/simple/QueryApplication.java
+++ anyline-simple-query/src/main/java/org/anyline/simple/QueryApplication.java
@@ -1,14 +1,5 @@
 package org.anyline.simple;
 
-import org.anyline.entity.DataRow;
-import org.anyline.entity.DataSet;
-import org.anyline.service.AnylineService;
-import org.anyline.util.DateUtil;
-import org.anyline.util.FileUtil;
-import org.anyline.util.MD5Util;
-import org.anyline.web.util.WebUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -16,9 +7,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.io.File;
-import java.util.Date;
-import java.util.List;
 
 
 @SpringBootApplication
