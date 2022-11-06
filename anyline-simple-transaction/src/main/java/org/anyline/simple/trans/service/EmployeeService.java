79ca0aa7f4b1b20253d6127ffbb987b104cf5372
anyline <server@anyline.org>
2022-11-06 11:14:43 +0800

    DefaultService

--- anyline-simple-transaction/src/main/java/org/anyline/simple/trans/service/EmployeeService.java
+++ anyline-simple-transaction/src/main/java/org/anyline/simple/trans/service/EmployeeService.java
@@ -1,12 +1,12 @@
 package org.anyline.simple.trans.service;
 
 import org.anyline.entity.DataRow;
-import org.anyline.service.init.SimpleService;
+import org.anyline.service.init.DefaultService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component("emp.service")
-public class EmployeeService extends SimpleService {
+public class EmployeeService extends DefaultService {
 
     @Transactional
     public void insert(DataRow row){
