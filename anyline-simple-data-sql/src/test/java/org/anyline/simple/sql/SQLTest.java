ef4b2492c9cffd5524b4dfcf00b48241234a8100
anyline <server@anyline.org>
2022-11-22 19:28:29 +0800

    java sql

--- anyline-simple-data-sql/src/test/java/org/anyline/simple/sql/SQLTest.java
+++ anyline-simple-data-sql/src/test/java/org/anyline/simple/sql/SQLTest.java
@@ -16,14 +16,43 @@ public class SQLTest {
 
     @Test
     public void init() throws Exception{
+
         //详细的查询条件构造方式 参考 anyline-simple-data-condition
+
+
+        //:PARAM_CODE 与 {PARAM_CODE} 效果一致但不能混用
+        //会生成占位符
         String sql = "SELECT * FROM CRM_USER WHERE CODE = :PARAM_CODE";
-        DataSet set = service.querys(sql, "PARAM_CODE:1");
+        DataSet set = service.querys(sql, "PARAM_CODE:100");
+        sql = "SELECT * FROM CRM_USER WHERE CODE = {PARAM_CODE}";
+        set = service.querys(sql, "PARAM_CODE:100");
+        //生成SQL SELECT * FROM CRM_USER WHERE CODE = ?
+
+
+
+        //::PARAM_CODE 与 ${PARAM_CODE} 效果一致但不能混用
+        //不生成占位符,而是在原sql上replace
+        //在一些比较复杂的情况,简单占位符胜任不了时 会用到
+        sql = "SELECT * FROM CRM_USER WHERE CODE = ::PARAM_CODE";
+        set = service.querys(sql, "PARAM_CODE:100");
+        sql = "SELECT * FROM CRM_USER WHERE CODE = ${PARAM_CODE}";
+        set = service.querys(sql, "PARAM_CODE:100");
         //生成SQL SELECT * FROM CRM_USER WHERE CODE = 1
 
-        //特别注意这以下情况 CODE:1与SQL中的变量匹配不成功时，SQL会追加一个条件 WHERE CODE = 1
+
+
+
+        //特别注意这以下情况 ID:1与SQL中的变量匹配不成功时，SQL会追加一个条件  ID = 1
         sql = "SELECT * FROM CRM_USER WHERE CODE = :PARAM_CODE";
-        set = service.querys(sql, "PARAM_CODE:1", "CODE:1");
-        //生成SQL SELECT * FROM CRM_USER WHERE CODE = 1 AND CODE = 1
+        set = service.querys(sql, "PARAM_CODE:1", "ID:1");
+        //生成SQL SELECT * FROM CRM_USER WHERE CODE = ? AND ID = ?
+
+
+        sql = "SELECT * FROM CRM_USER WHERE CODE = :PARAM_CODE";
+        set = service.querys(sql);
+
+        //但是以个SQL 最好应该这样写
+        //service.query("CRM_USER", "CODE:100");
+
     }
 }
