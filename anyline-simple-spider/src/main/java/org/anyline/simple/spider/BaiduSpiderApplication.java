1a1870fd66b3e02944fe332fa94c72ddc3b141d1
anyline <server@anyline.org>
2023-01-11 17:17:14 +0800

    baidu site

--- anyline-simple-spider/src/main/java/org/anyline/simple/spider/BaiduSpiderApplication.java
+++ anyline-simple-spider/src/main/java/org/anyline/simple/spider/BaiduSpiderApplication.java
@@ -1,22 +1,10 @@
 package org.anyline.simple.spider;
 
-import org.anyline.baidu.site.util.BaiduSiteClient;
-import org.anyline.baidu.site.util.BaiduSiteConfig;
+import org.anyline.baidu.site.util.BaiduSeoClient;
+import org.anyline.baidu.site.util.BaiduSeoConfig;
 import org.anyline.baidu.site.util.SubmitResponse;
-import org.anyline.net.HttpResponse;
-import org.anyline.net.HttpUtil;
-import org.anyline.spider.SpiderClient;
-import org.apache.http.entity.StringEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.script.*;
-import java.io.File;
-import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 public class BaiduSpiderApplication {
@@ -32,8 +20,8 @@ public class BaiduSpiderApplication {
         //https://ziyuan.baidu.com/linksubmit/index?site=https://www.anyline.org
         String site = "https://www.anyline.org";
         String token = "";
-        BaiduSiteConfig.register(site, token);
-        BaiduSiteClient client = BaiduSiteClient.getInstance();
+        BaiduSeoConfig.register(site, token);
+        BaiduSeoClient client = BaiduSeoClient.getInstance();
         List<String> urls = new ArrayList<>();
         urls.add("https://www.anyline.org");
         SubmitResponse response = client.submit(urls);
