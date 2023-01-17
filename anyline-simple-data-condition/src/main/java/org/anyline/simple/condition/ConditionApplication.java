492cf3dea429a10726c38f50fc6b2e1be0af0672
anyline <server@anyline.org>
2023-01-17 11:17:59 +0800

    doc

---
+++ anyline-simple-data-condition/src/main/java/org/anyline/simple/condition/ConditionApplication.java
@@ -0,0 +1,25 @@
+package org.anyline.simple.condition;
+
+import org.anyline.data.jdbc.ds.DynamicDataSourceRegister;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.anyline"})
+@Import(DynamicDataSourceRegister.class)
+public class ConditionApplication {
+
+	private static Logger log = LoggerFactory.getLogger(ConditionApplication.class);
+	public static void main(String[] args) {
+
+		SpringApplication application = new SpringApplication(ConditionApplication.class);
+
+		ConfigurableApplicationContext context = application.run(args);
+
+	}
+}
