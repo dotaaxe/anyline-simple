377acd4e5326adf91dcbb60e6831a4e1d0c1c77e
anyline <server@anyline.org>
2023-01-21 23:30:16 +0800

    coordinate doc

--- anyline-simple-map/src/main/java/org.anyline.simple.map/MapApplication.java
+++ anyline-simple-map/src/main/java/org.anyline.simple.map/MapApplication.java
@@ -4,7 +4,6 @@ package org.anyline.simple.map;
 import org.anyline.entity.Coordinate;
 import org.anyline.map.MapProxy;
 import org.anyline.util.BeanUtil;
-import org.anyline.util.GISUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -15,9 +14,13 @@ import org.springframework.context.annotation.ComponentScan;
 public class MapApplication {
 
     public static void main(String[] args) {
+        //注意直辖市的区级(相当于省下的市级)返回编号与国家统计局编码不一致,但再下一级一致
+        // 如北京朝阳区 统计局：110105  地图接口返回1101
+        // 因为在统计局编码中 北京市一级是 市辖区 1101  再下一级是朝阳区
+        // 一般需要查出最低一级编码后与标签库对比后修正上几级编码
         SpringApplication application = new SpringApplication(MapApplication.class);
         ConfigurableApplicationContext context = application.run(args);
-        Coordinate coordinate = MapProxy.regeo(Coordinate.TYPE.GCJ02LL, "120.378355","36.06256");
+        Coordinate coordinate = MapProxy.regeo(Coordinate.TYPE.BD09LL, "115.9893280","40.4673940");
         System.out.println(BeanUtil.object2json(coordinate));
     }
 }
