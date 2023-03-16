d03490fd8f493421844640fff492944896494dc1
anyline <server@anyline.org>
2023-03-17 02:09:40 +0800

    query interceptor

---
+++ anyline-simple-data-jdbc-interceptor/src/main/java/org/anyline/simple/interceptor/InterceptorApplication.java
@@ -0,0 +1,23 @@
+package org.anyline.simple.interceptor;
+
+
+import org.anyline.service.AnylineService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.anyline"})
+public class InterceptorApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication application = new SpringApplication(InterceptorApplication.class);
+        ConfigurableApplicationContext context = application.run(args);
+
+        AnylineService service = context.getBean(AnylineService.class);
+        service.query("CRM_USER");
+
+    }
+}
