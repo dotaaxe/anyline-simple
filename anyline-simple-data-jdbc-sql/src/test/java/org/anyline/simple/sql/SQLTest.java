3db8cb021f0736c5c78ff7cb65ba8304c5d10c91
anyline <server@anyline.org>
2023-04-21 14:23:53 +0800

    text sql

--- anyline-simple-data-jdbc-sql/src/test/java/org/anyline/simple/sql/SQLTest.java
+++ anyline-simple-data-jdbc-sql/src/test/java/org/anyline/simple/sql/SQLTest.java
@@ -2,6 +2,7 @@ package org.anyline.simple.sql;
 
 import org.anyline.data.param.ConfigStore;
 import org.anyline.data.param.init.DefaultConfigStore;
+import org.anyline.entity.Compare;
 import org.anyline.entity.DataRow;
 import org.anyline.entity.DataSet;
 import org.anyline.service.AnylineService;
@@ -29,14 +30,40 @@ public class SQLTest {
      ************************************************************************************************************/
     @Test
     public void init() throws Exception{
-
+        ConfigStore conditions = new DefaultConfigStore();
+        conditions.param("PARAM_CODE", 222);
         //详细的查询条件构造方式 参考 anyline-simple-data-condition
         ConfigTable.IS_AUTO_CHECK_METADATA = true;
+
+
+        ConfigStore configs = new DefaultConfigStore();
+        configs.and(Compare.GREAT, "ID", "1");
+        configs.and(Compare.LESS, "ID", "5");
+        service.querys("SELECT * FROM CRM_USER", configs);
+        // SELECT * FROM CRM_USER WHERE ID > 1 AND ID < 5
+
+        DataRow user = service.query("CRM_USER", configs);
+        // SELECT * FROM CRM_USER WHERE ID > 1 AND ID < 5
+
+        service.update(user, configs);
+        // UPDATE CRM_USER SET ... WHERE ID > 1 AND ID < 5
+
+        conditions = new DefaultConfigStore();
+        String sql = "SELECT * FROM CRM_USER WHERE CODE = :CODE";
+        conditions.and("CODE", "100");
+        service.querys(sql, conditions);
+        //SELECT * FROM CRM_USER WHERE CODE = ?
+
+        sql = "SELECT * FROM CRM_USER WHERE ID IN(:IDS)";
+        conditions.param("IDS", "1,2,3".split(","));
+        service.querys(sql, conditions);
+        //SELECT * FROM CRM_USER WHERE ID IN(?,?,?)
+
         //:PARAM_CODE 与 {PARAM_CODE} 效果一致但不能混用
         //会生成占位符 "PARAM_CODE:100" 与SQL中的占位符能匹配成功 会把值100赋值给占位符
-        String sql = "SELECT * FROM CRM_USER WHERE CODE = :PARAM_CODE AND NAME != :PARAM_CODE AND FIND_IN_SET(:PARAM_CODE, CODE)";
+        sql = "SELECT * FROM CRM_USER WHERE CODE = :PARAM_CODE AND NAME != :PARAM_CODE AND FIND_IN_SET(:PARAM_CODE, CODE)";
+        service.querys(sql, conditions);
         service.querys(sql, "PARAM_CODE:111");
-        service.querys(sql, new DefaultConfigStore().param("PARAM_CODE", 222));
 
         sql = "SELECT * FROM CRM_USER WHERE CODE = #{PARAM_CODE}";
         service.querys(sql, "PARAM_CODE:100");
@@ -53,7 +80,7 @@ public class SQLTest {
 
         sql = "SELECT * FROM CRM_USER WHERE CODE = ${PARAM_CODE}";
         service.querys(sql, "PARAM_CODE:100");
-        //生成SQL SELECT * FROM CRM_USER WHERE CODE = 1
+        //生成SQL SELECT * FROM CRM_USER WHERE CODE = 100
 
 
         //特别注意这以下情况 ID:1与SQL中的变量匹配不成功时，SQL会追加一个条件  ID = 1
@@ -63,7 +90,7 @@ public class SQLTest {
 
 
         //相当于web环境中的ConfigStore configs = condition()
-        ConfigStore configs = new DefaultConfigStore();
+        configs = new DefaultConfigStore();
         Map<String,Object> map = new HashMap<>();
         map.put("ID", "100");
         configs.setValue(map); //这里相当于接收request的提交的参数
