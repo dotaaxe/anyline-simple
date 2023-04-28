886d9728ffc3eba557eadba1af60e3fd728ffe23
anyline <server@anyline.org>
2023-04-28 17:13:33 +0800

    InitTest

--- anyline-simple-alpha-init/src/test/java/org/anyline/simple/init/InitTest.java
+++ anyline-simple-alpha-init/src/test/java/org/anyline/simple/init/InitTest.java
@@ -2,7 +2,7 @@ package org.anyline.simple.init;
 
 import org.anyline.entity.DataRow;
 import org.anyline.entity.DataSet;
-import org.anyline.data.jdbc.adapter.JDBCAdapter;
+import org.anyline.data.adapter.JDBCAdapter;
 import org.anyline.data.entity.Table;
 import org.anyline.service.AnylineService;
 import org.junit.jupiter.api.Assertions;
