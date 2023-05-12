d7f53e996345bd7876bb7e2b08af49d0eb527368
anyline <server@anyline.org>
2023-05-12 23:05:40 +0800

    UserController

--- anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/controller/DSController.java
+++ anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/controller/DSController.java
@@ -1,31 +1,37 @@
 package org.anyline.simple.controller;
 
 import org.anyline.controller.impl.AnylineController;
+import org.anyline.data.jdbc.ds.DataSourceHolder;
 import org.anyline.entity.DataRow;
-import org.anyline.simple.service.EmployeeService;
-import org.anyline.simple.service.UserService;
+import org.anyline.simple.service.DSService;
+import org.anyline.simple.service.SSOService;
 import org.anyline.util.BasicUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
-@RequestMapping("usr")
-public class UserController extends AnylineController {
+@RequestMapping("ds")
+public class DSController extends AnylineController {
 
     @Autowired
-    private UserService service;
+    private DSService service;
+
     @RequestMapping("a")
     public String add(){
-        int qty = service.count("<sso>SSO_USER");
+        String ds = "sso";
+        DataSourceHolder.setDataSource(ds);
+        int qty = service.count("SSO_USER");
         try {
             DataRow row = new DataRow();
             row.put("NM", BasicUtil.getRandomString(10));
-            service.insert(row);
+            service.insert(ds, row);
         }catch (Exception e){
             e.printStackTrace();
         }
-        int cnt = service.count("<sso>SSO_USER");
+        int cnt = service.count("SSO_USER");
         return success("insert前后行数:"+qty+">"+cnt);
     }
 }
