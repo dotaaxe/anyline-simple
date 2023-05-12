76842444e34d0b9c77a4a7775f78b18f380aa8e8
anyline <server@anyline.org>
2023-05-13 07:33:04 +0800

    DefaultService

--- anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/service/DefaultService.java
+++ anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/service/DefaultService.java
@@ -1,14 +1,15 @@
 package org.anyline.simple.service;
 
+import org.anyline.data.jdbc.ds.DataSourceHolder;
 import org.anyline.entity.DataRow;
-import org.anyline.service.init.DefaultService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Component("emp.service")
-public class EmployeeService extends DefaultService {
-    @Transactional
+@Component("def.service")
+public class DefaultService extends org.anyline.service.init.DefaultService {
+    @Transactional(value = "transactionManager")
     public void insert(DataRow row){
+        DataSourceHolder.startTransaction();
         insert("HR_EMPLOYEE", row);
         throw new RuntimeException("test exception");
     }
