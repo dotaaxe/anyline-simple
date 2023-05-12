6cad9d8e4c354e44189d4f554350fef90edd6fcf
anyline <server@anyline.org>
2023-05-13 07:33:12 +0800

    DefaultController

--- anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/controller/DefaultController.java
+++ anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/controller/DefaultController.java
@@ -2,24 +2,24 @@ package org.anyline.simple.controller;
 
 import org.anyline.controller.impl.AnylineController;
 import org.anyline.entity.DataRow;
-import org.anyline.simple.service.EmployeeService;
+import org.anyline.simple.service.DefaultService;
 import org.anyline.util.BasicUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("emp")
-public class EmployeeController extends AnylineController {
+@RequestMapping("def")
+public class DefaultController extends AnylineController {
 
     @Autowired
-    private EmployeeService service;
+    private DefaultService service;
     @RequestMapping("a")
     public String add(){
         int qty = service.count("HR_EMPLOYEE");
         try {
             DataRow row = new DataRow();
-            row.put("NM", BasicUtil.getRandomString(10));
+            row.put("NAME", BasicUtil.getRandomString(10));
             service.insert(row);
         }catch (Exception e){
             e.printStackTrace();
