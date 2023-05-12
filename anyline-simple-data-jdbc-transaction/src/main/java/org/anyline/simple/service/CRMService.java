a6f8eded07052e52f7de609edaa58b2ee1701ec7
anyline <server@anyline.org>
2023-05-13 07:33:38 +0800

    CRMService

--- anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/service/CRMService.java
+++ anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/service/CRMService.java
@@ -5,9 +5,9 @@ import org.anyline.service.init.DefaultService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Component("cust.service")
-public class CustomerService extends DefaultService {
-    @Transactional
+@Component("crm.service")
+public class CRMService extends DefaultService {
+    @Transactional(value = "anyline.transaction.crm")
     public void insert(DataRow row){
         insert("<crm>crm_customer", row);
         throw new RuntimeException("test exception");
