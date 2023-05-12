f2db015dde42f268d2ca5fb392cdb07c28a60f35
anyline <server@anyline.org>
2023-05-12 23:04:44 +0800

    SSOController

---
+++ anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/controller/SSOController.java
@@ -0,0 +1,33 @@
+package org.anyline.simple.controller;
+
+import org.anyline.controller.impl.AnylineController;
+import org.anyline.entity.DataRow;
+import org.anyline.simple.service.SSOService;
+import org.anyline.util.BasicUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("sso")
+public class SSOController extends AnylineController {
+
+    @Autowired
+    private SSOService service;
+
+    @RequestMapping("a")
+    public String add(HttpServletRequest request){
+        int qty = service.count("<sso>SSO_USER");
+        try {
+            DataRow row = new DataRow();
+            row.put("NM", BasicUtil.getRandomString(10));
+            service.insert(row);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        int cnt = service.count("<sso>SSO_USER");
+        return success("insert前后行数:"+qty+">"+cnt);
+    }
+}
