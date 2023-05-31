37a4ac08b91b1b61f68f593734c6dc76e57ca52b
anyline <server@anyline.org>
2023-05-31 23:50:16 +0800

    编程事务示例

--- anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/service/DSService.java
+++ anyline-simple-data-jdbc-transaction/src/main/java/org/anyline/simple/service/DSService.java
@@ -2,12 +2,12 @@ package org.anyline.simple.service;
 
 import org.anyline.data.jdbc.ds.DataSourceHolder;
 import org.anyline.entity.DataRow;
+import org.anyline.proxy.ServiceProxy;
 import org.anyline.service.AnylineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component("ds.service")
 public class DSService {
@@ -16,17 +16,31 @@ public class DSService {
     @Qualifier("anyline.service")
     protected AnylineService service;
 
-    public void insert(String ds, DataRow row, int flag){
+    //通用数据源(就是可以执行多个数据源，可切换)
+    public void insert(String ds, DataRow row){
         DataSourceHolder.setDataSource(ds);
-        TransactionStatus status = DataSourceHolder.startTransaction();
-        service.insert("SSO_USER", row);
-        if(flag == 0) {
+        TransactionStatus status = DataSourceHolder.startTransaction();//这里没有指定数据源 会默认当前数据源
+        try {
+            service.insert("SSO_USER", row);
+            DataSourceHolder.commit(status);//这里不需要指定数据源，因为status已经绑定了数据源
+        }catch (Exception e){
             DataSourceHolder.rollback(status);
-        }else{
-            DataSourceHolder.commit(status);
         }
     }
 
+    //固定数据源(就是为每个数据源生成独立的jdbcTemplate,事务管理器,dao,service)
+    //推荐这种方式，可以避免线程共享的引起的切换失败问题
+    public void inserts(String ds, DataRow row){
+        AnylineService service = ServiceProxy.service(ds); //返回操作固定数据源的service
+        TransactionStatus status = DataSourceHolder.startTransaction(ds);
+        try {
+            service.insert("SSO_USER", row);
+            DataSourceHolder.commit(status); //这里不需要指定数据源，因为status已经绑定了数据源
+        }catch (Exception e){
+
+            DataSourceHolder.rollback(status);
+        }
+    }
     public int count(String table){
         return service.count(table);
     }
