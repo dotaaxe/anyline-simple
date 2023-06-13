210bb5762e259aa8b38bbfabd769e5a04f5798ac
anyline <server@anyline.org>
2023-06-14 00:04:36 +0800

    DmApplication

--- anyline-simple-data-jdbc-dialect/anyline-simple-data-jdbc-dm/src/main/java/org/anyline/simple/DmApplication.java
+++ anyline-simple-data-jdbc-dialect/anyline-simple-data-jdbc-dm/src/main/java/org/anyline/simple/DmApplication.java
@@ -1,23 +1,13 @@
 package org.anyline.simple;
 
-import org.anyline.entity.DataSet;
-import org.anyline.service.AnylineService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(DmApplication.class);
-
-		ConfigurableApplicationContext context = application.run(args);
-		AnylineService service = (AnylineService) context.getBean("anyline.service");
-		//DataSourceHolder.setDataSource("jt");
-
-		DataSet set = service.querys("all_tables",0,10);
-		set = service.querys("all_tables",0,10);
-		System.out.println(set);
+		application.run(args);
 	}
 }
