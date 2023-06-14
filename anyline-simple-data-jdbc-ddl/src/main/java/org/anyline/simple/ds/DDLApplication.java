7efec6d1413777ba46f22b1299e0f1e99d7fa75e
anyline <server@anyline.org>
2023-06-14 13:32:47 +0800

    doc

--- anyline-simple-data-jdbc-ddl/src/main/java/org/anyline/simple/ds/DDLApplication.java
+++ anyline-simple-data-jdbc-ddl/src/main/java/org/anyline/simple/ds/DDLApplication.java
@@ -2,16 +2,12 @@ package org.anyline.simple.ds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.anyline"})
-//
 public class DDLApplication {
 
 
-	public static void main(String[] args) throws Exception{
-
+	public static void main(String[] args){
 		SpringApplication application = new SpringApplication(DDLApplication.class);
 		application.run(args);
 
