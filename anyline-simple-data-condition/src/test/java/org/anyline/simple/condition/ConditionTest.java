c53491e6f1ab04aa888a3d93ed1938a080488ac5
anyline <server@anyline.org>
2023-07-24 19:29:17 +0800

    in append values

--- anyline-simple-data-condition/src/test/java/org/anyline/simple/condition/ConditionTest.java
+++ anyline-simple-data-condition/src/test/java/org/anyline/simple/condition/ConditionTest.java
@@ -2,6 +2,7 @@ package org.anyline.simple.condition;
 
 import org.anyline.data.param.ConfigStore;
 import org.anyline.data.param.init.DefaultConfigStore;
+import org.anyline.entity.Compare;
 import org.anyline.service.AnylineService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,66 @@ public class ConditionTest {
     @Autowired
     private AnylineService service          ;
 
+    @Test
+    public void over(){
+        ConfigStore configs = new DefaultConfigStore();
+        configs.and("ID", "1");
+        configs.and("ID","2");
+        configs.and("ID","3");
+        service.query("crm_user", configs);
+        //WHERE(ID = 1 AND  ID = 2 AND ID = 3)
+
+        // overCondition 覆盖key相同并且运行符相同的条件,true在现有条件基础上修改(多个相同key的条件只留下第一个),false:添加新条件
+        // overValue	 覆盖key相同并且运行符相同的条件时，是否覆盖条件值,如果不覆盖则与原来的值合成新的集合
+        configs = new DefaultConfigStore();
+        configs.and("ID", "1");
+        configs.and("ID", "2");
+        configs.and("ID","3", true, true);
+        service.query("crm_user", configs);
+        //WHERE(ID = 3)
+        //因为最后一步要覆盖相同的条件,也就是相同的条件中只留下第一个，并且用3覆盖原有的值（这里留哪个没影响，因为他的值最后都会被3覆盖）
+
+        configs = new DefaultConfigStore();
+        configs.and("ID", "1");
+        configs.and("ID", "2");
+        configs.and("ID","3", true, false);
+        service.query("crm_user", configs);
+        //WHERE(ID = 1)
+        //因为最后一步要覆盖相同的条件，也就是相同的条件中只留下第一个，但并不覆盖值，所以并没有用3覆盖1
+
+
+        configs = new DefaultConfigStore();
+        configs.and("ID", "1,2".split(","));
+        service.query("crm_user", configs);
+        //WHERE ID IN(1,2)
+
+        configs = new DefaultConfigStore();
+        configs.and("ID", "1,2".split(",")); //这里会生成like
+        configs.and("ID","3", true, true); //因为运算符不一致所以不会覆盖
+        service.query("crm_user", configs);
+        //WHERE(ID IN (1,2) AND  ID = 3)
+
+        configs = new DefaultConfigStore();
+        configs.and("ID", "1,2".split(",")); //这里会生成like
+        configs.and(Compare.IN, "ID","3", true, true); //因为KEY和运算符都一致所以会覆盖
+        service.query("crm_user", configs);
+        //WHERE(ID IN (3))
+
+
+        /***************************************************************************************************************
+         *
+         *                                       注意这里的 overValue = false
+         *                                       虽然不会覆盖值，
+         *                                       因为运算符IN可以接收多个值
+         *                                       所有会在原来的基础上追加形成[1,2,3]
+         *
+         * ************************************************************************************************************/
+        configs = new DefaultConfigStore();
+        configs.and("ID", "1,2".split(",")); //这里会生成like
+        configs.and(Compare.IN, "ID","3", true, false);
+        service.query("crm_user", configs);
+        //WHERE(ID IN (1,2,3))
+    }
     @Test
     public void or(){
         ConfigStore configs = new DefaultConfigStore();
