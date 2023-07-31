b8059c0db790e88b61c3d776613c38d58d2d8e0f
anyline <server@anyline.org>
2023-07-31 19:06:16 +0800

    hive

--- anyline-simple-data-jdbc-dialect/anyline-simple-data-jdbc-hive/src/main/java/org/anyline/simple/hive/HiveApplication.java
+++ anyline-simple-data-jdbc-dialect/anyline-simple-data-jdbc-hive/src/main/java/org/anyline/simple/hive/HiveApplication.java
@@ -1,5 +1,6 @@
 package org.anyline.simple.hive;
 
+import org.anyline.entity.DataRow;
 import org.anyline.entity.DataSet;
 import org.anyline.service.AnylineService;
 import org.springframework.boot.SpringApplication;
@@ -13,10 +14,17 @@ public class HiveApplication {
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(HiveApplication.class);
 		ConfigurableApplicationContext context = application.run(args);
-		org.slf4j.impl.StaticLoggerBinder s;
 		AnylineService service = (AnylineService) context.getBean("anyline.service");
 		String sql = "${CREATE TABLE crm_user(ID INT, NAME varchar(100))}";
 		//service.execute(sql);
+		DataSet users = new DataSet();
+		for(int i=0; i<20;i++) {
+			DataRow user = new DataRow();
+			user.put("ID", (i+1));
+			user.put("NAME", "USER_"+(i+1));
+			users.add(user);
+		}
+		service.insert("crm_user", users);
 		DataSet set = service.querys("crm_user",0,10);
 		System.out.println(set);
 	}
