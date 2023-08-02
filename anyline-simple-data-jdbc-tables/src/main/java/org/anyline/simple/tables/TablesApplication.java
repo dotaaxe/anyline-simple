346ea6d80b3427ba953beb367ea230ea582fca79
anyline <server@anyline.org>
2023-08-02 09:46:26 +0800

    tables querys

--- anyline-simple-data-jdbc-tables/src/main/java/org/anyline/simple/tables/TablesApplication.java
+++ anyline-simple-data-jdbc-tables/src/main/java/org/anyline/simple/tables/TablesApplication.java
@@ -1,7 +1,9 @@
 package org.anyline.simple.tables;
 
-import org.anyline.entity.DataSet;
+import org.anyline.data.param.ConfigStore;
 import org.anyline.data.param.TableBuilder;
+import org.anyline.data.param.init.DefaultConfigStore;
+import org.anyline.entity.DataSet;
 import org.anyline.service.AnylineService;
 import org.anyline.util.ConfigTable;
 import org.springframework.boot.SpringApplication;
@@ -40,6 +42,17 @@ public class TablesApplication {
 		service.querys("SELECT E.ID AS EMPLOYEE_ID, D.NM AS DEPT_NM FROM HR_EMPLOYEE AS E LEFT JOIN HR_DEPARTMENT AS D ON E.DEPARTMENT_ID = D.ID"
 		,"E.ID:>=1", "E.NM:张%");
 
+		//如果查的列比较多可以通过ConfigStore来设置
+		ConfigStore configs = new DefaultConfigStore();
+		configs.columns("M.ID","F.CODE AS DEPARTMENT_CODE");
+		service.query(TableBuilder.init("HR_EMPLOYEE AS M").left("HR_DEPARTMENT AS F", "F.DEPARTMENT_ID = F.ID").build(), configs);
+		/*SELECT
+			M.ID,F.CODE AS DEPARTMENT_CODE
+		FROM
+			HR_EMPLOYEE
+		  M
+			LEFT JOIN HR_DEPARTMENT  F ON F.DEPARTMENT_ID = F.ID
+	   */
 		System.exit(0);
 	}
 
