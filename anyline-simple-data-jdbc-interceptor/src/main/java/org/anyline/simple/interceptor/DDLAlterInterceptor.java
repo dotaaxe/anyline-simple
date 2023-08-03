f61f614837d8223c02daddf37186cd8a665f2778
anyline <server@anyline.org>
2023-08-03 21:47:39 +0800

    DDLAlterInterceptor

--- anyline-simple-data-jdbc-interceptor/src/main/java/org/anyline/simple/interceptor/DDLAlterInterceptor.java
+++ anyline-simple-data-jdbc-interceptor/src/main/java/org/anyline/simple/interceptor/DDLAlterInterceptor.java
@@ -1,8 +1,8 @@
 package org.anyline.simple.interceptor;
 
 import org.anyline.data.interceptor.DDInterceptor;
-import org.anyline.data.jdbc.ds.JDBCRuntime;
 import org.anyline.data.run.Run;
+import org.anyline.data.runtime.DataRuntime;
 import org.anyline.metadata.ACTION;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +12,7 @@ import java.util.List;
 @Component
 public class DDLAlterInterceptor implements DDInterceptor {
     @Override
-    public ACTION.SWITCH before(JDBCRuntime runtime, String random, ACTION.DDL action, Object metadata, List<Run> runs) {
+    public ACTION.SWITCH before(DataRuntime runtime, String random, ACTION.DDL action, Object metadata, List<Run> runs) {
         for (Run run:runs){
             String sql = run.getFinalUpdate();
             System.out.println("--------------DDLAlterInterceptor("+action+")-------------------\n"+sql);
