5268fff294c1063a6b4f22ddd3eb159a38f45409
anyline <server@anyline.org>
2023-08-04 23:39:28 +0800

    DMLQueryInterceptor

--- anyline-simple-data-jdbc-interceptor/src/main/java/org/anyline/simple/interceptor/DMLQueryInterceptor.java
+++ anyline-simple-data-jdbc-interceptor/src/main/java/org/anyline/simple/interceptor/DMLQueryInterceptor.java
@@ -3,7 +3,9 @@ package org.anyline.simple.interceptor;
 import org.anyline.data.interceptor.QueryInterceptor;
 import org.anyline.data.param.ConfigStore;
 import org.anyline.data.prepare.RunPrepare;
+import org.anyline.data.run.Run;
 import org.anyline.data.runtime.DataRuntime;
+import org.anyline.entity.PageNavi;
 import org.anyline.metadata.ACTION;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,13 @@ public class DMLQueryInterceptor implements QueryInterceptor {
     @Override
     public ACTION.SWITCH prepare(DataRuntime runtime, RunPrepare prepare, ConfigStore configs, String... conditions) {
         configs.and("ID", 1);
+        return ACTION.SWITCH.CONTINUE;
+    }
+
+    @Override
+    public ACTION.SWITCH after(DataRuntime runtime, Run run, boolean success, Object result, PageNavi navi, long millis){
+        String sql = run.getFinalQuery();
+        System.out.println(sql);
         return ACTION.SWITCH.CONTINUE;
     }
 
